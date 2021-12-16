bc141e5bdffe3923d1a1f2fd4f80ff71e73ef39f
sndrgb <alessandro@sixsocks.studio>
2021-12-16 15:50:18 +0100

    logger function take more args

--- logger.js
+++ logger.js
@@ -2,18 +2,24 @@ const old = console.log;
 const logger = document.querySelector('#logger');
 let counter = 0
 
-console.log = (message) => {
+console.log = (...args) => {
   counter++
-  let log = message
 
-  if (typeof message === 'object') {
-    log = JSON.stringify(message);
-  }
+  let log = args.reduce((oldVal, val) => {
+    let newValue = val
+
+    if (typeof newValue === 'object') {
+      newValue = JSON.stringify(newValue);
+    }
+
+    return `${oldVal} ${newValue}`;
+
+  })
 
   const row = document.createElement('pre');
   const text = document.createTextNode(`(${counter}): ${log}`);
   row.appendChild(text);
   logger.appendChild(row);
 
-  old(message);
+  old(...args);
 }
